14f82405e21d325610f5a47d093884b2f944b12b
Sudhakar Reddy <sudhakarsjce@gmail.com>
2014-05-01 10:55:46 -0700

    added screenshot, changed view name

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 A simple search application built completely with AngularJS on MarkLogic (NoSql) database
 
+![oscars](app-screenshot.png?raw=true "App home page")
 
--- app/js/app.js
+++ app/js/app.js
@@ -12,9 +12,9 @@ var app = angular.module('myApp', [
 ]);
 
 app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MainCtrl'});
+  $routeProvider.when('/home', {templateUrl: 'partials/partial1.html', controller: 'MainCtrl'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 	$httpProvider.defaults.withCredentials = true;
 	 $httpProvider.defaults.headers.post = {};
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
